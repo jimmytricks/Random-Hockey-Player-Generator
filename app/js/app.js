fc61e3059a181b50b9e557d90416e72faa657928
Hixie4 <jhixie4@gmail.com>
2018-12-02 20:50:46 -0800

    added comments, cleaned some code

--- app/js/app.js
+++ app/js/app.js
@@ -2,19 +2,39 @@ const config = {
   apiInfo: 'https://statsapi.web.nhl.com/api/v1/teams/?expand=team.roster&teamId=',
 }
 
+/* set event listeners */
 let leftInitElement = document.getElementById('left-init');
-leftInitElement.addEventListener("click",init);
+let rightInitElement = document.getElementById('right-init');
+leftInitElement.addEventListener("click", init);
+rightInitElement.addEventListener("click", init);
 
-let usedNumbers = [];
-let usedNumbers2 = [];
+// Set arrays for left and right
+let usedNumbersLeft = [];
+let usedNumbersRight = [];
 
 function init(e) {
-  debugger;
+
+  // Set left or right based on target button
+  let leftOrRight;
+  let isLeftSelectedBoolean = false;
+  if (e.target.id == "left-init") {
+    leftOrRight = 'left';
+    isLeftSelectedBoolean = true;
+  } else {
+    leftOrRight = 'right';
+  }
+
+  // Set team selector and name to use in DOM
+  let setTeamSelectorID = `team-selector-${leftOrRight}`;
+  let setNameID = `name-${leftOrRight}`;
+
+  // check if left or right team array should be set
+  let specificUsedNumberArray = isLeftSelectedBoolean ? usedNumbersLeft : usedNumbersRight;
 
   let teamToGet = Number(
-    document.getElementById("team-selector").value);
+    document.getElementById(setTeamSelectorID).value);
 
-  let nameOfPerson = document.getElementById('name').value;
+  let nameOfPerson = document.getElementById(setNameID).value;
 
   fetch(`${config.apiInfo}${teamToGet}`)
     .then(
@@ -36,8 +56,6 @@ function init(e) {
       console.log('Fetch Error :-S', err);
     });
 
-
-
   function pushToArray(teamArray) {
     let teamArrayRoster = teamArray.teams[0].roster.roster;
     return teamArrayRoster;
@@ -46,18 +64,15 @@ function init(e) {
   function getRandomPlayer(teamArray) {
     let arrayLength = teamArray.length;
     let randomSelection = calcRandomNumber(arrayLength - 1);
-    usedNumbers.push(randomSelection);
+    specificUsedNumberArray.push(randomSelection);
     let selectedPlayer = teamArray[randomSelection];
-    console.log(`${nameOfPerson} - ${selectedPlayer.jerseyNumber} - ${selectedPlayer.person.fullName} - ${selectedPlayer.position.name}`);
     return selectedPlayer;
   }
 
   function calcRandomNumber(amountOfPlayers) {
     let counter = 0;
     let randomSelection = Math.floor(Math.random() * Math.floor(amountOfPlayers));
-
-
-    if (usedNumbers.includes(randomSelection) && counter <= amountOfPlayers) {
+    if (specificUsedNumberArray.includes(randomSelection) && counter <= amountOfPlayers) {
       console.log(randomSelection + ' looping recursion' + counter);
       counter += 1;
       calcRandomNumber(amountOfPlayers);
@@ -66,88 +81,14 @@ function init(e) {
       console.log('done with recursion' + counter)
       return randomSelection;
     }
-
   }
 
   function showInDom(selectedPlayer) {
     let textOutput = `${nameOfPerson}: ${selectedPlayer.person.fullName} #${selectedPlayer.jerseyNumber} - ${selectedPlayer.position.name}`;
 
-    let mainSectionDOM = document.getElementById('main-left-side');
-    let newUl = document.createElement('ul');
-    let newLi = document.createElement('li');
+    let specificSectionDOM = `main-${leftOrRight}-side`;
 
-    mainSectionDOM.appendChild(newUl);
-    newUl.appendChild(newLi);
-
-    let newTextNode = document.createTextNode(textOutput);
-    newLi.appendChild(newTextNode);
-  }
-}
-
-function init2() {
-
-  let teamToGet = Number(
-    document.getElementById("team-selector2").value);
-
-  let nameOfPerson = document.getElementById('name2').value;
-
-  fetch(`${config.apiInfo}${teamToGet}`)
-    .then(
-      function (response) {
-        if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-          return;
-        }
-
-        // Examine the text in the response
-        response.json()
-          .then(pushToArray)
-          .then(getRandomPlayer)
-          .then(showInDom);
-      }
-    )
-    .catch(function (err) {
-      console.log('Fetch Error :-S', err);
-    });
-
-
-
-  function pushToArray(teamArray) {
-    let teamArrayRoster = teamArray.teams[0].roster.roster;
-    return teamArrayRoster;
-  }
-
-  function getRandomPlayer(teamArray) {
-    let arrayLength = teamArray.length;
-    let randomSelection = calcRandomNumber(arrayLength - 1);
-    usedNumbers2.push(randomSelection);
-    let selectedPlayer = teamArray[randomSelection];
-    console.log(`${nameOfPerson} - ${selectedPlayer.jerseyNumber} - ${selectedPlayer.person.fullName} - ${selectedPlayer.position.name}`);
-    return selectedPlayer;
-  }
-
-  function calcRandomNumber(amountOfPlayers) {
-    let counter = 0;
-    let randomSelection = Math.floor(Math.random() * Math.floor(amountOfPlayers));
-
-
-    if (usedNumbers2.includes(randomSelection) && counter <= amountOfPlayers) {
-      console.log(randomSelection + ' looping recursion' + counter);
-      counter += 1;
-      calcRandomNumber(amountOfPlayers);
-    } else {
-      counter = 0;
-      console.log('done with recursion' + counter)
-      return randomSelection;
-    }
-
-  }
-
-  function showInDom(selectedPlayer) {
-    let textOutput = `${nameOfPerson}: ${selectedPlayer.person.fullName} #${selectedPlayer.jerseyNumber} - ${selectedPlayer.position.name}`;
-
-    let mainSectionDOM = document.getElementById('main-right-side');
+    let mainSectionDOM = document.getElementById(specificSectionDOM);
     let newUl = document.createElement('ul');
     let newLi = document.createElement('li');
 
